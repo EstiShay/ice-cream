d54b6fe4acc8b167a0e4cd9c998837fe1a2e46da
Esti Shay and Stephanie Mayer <esti@>
2017-07-11 09:34:35 -0700

    refactor for each as for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(function(){
 
   var flavors = ["rocky road", "cookie dough", "marionberry", "salted caramel", "balsamic vinagrette"];
-  flavors.forEach(function(flavor) {
-  //        debugger;
-    $("ul").append("<li>" + flavor);
-  });
+  // flavors.forEach(function(flavor) {
+  //   $("ul").append("<li>" + flavor);
+  for (var index = 0; index < flavors.length; index += 1) {
+    $("ul").append("<li>" + flavors[index]);
+  }
 });
